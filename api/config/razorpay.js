6d21d4a0d8c8801010d90d1757b1dd82adf0f809
Thamir <thamirsiddik@gmail.com>
2025-06-27 17:27:35 +0530

    Debug razor pay key

--- api/config/razorpay.js
+++ api/config/razorpay.js
@@ -1,6 +1,9 @@
 import Razorpay from "razorpay";
 import { RAZORPAY_API_KEY, RAZORPAY_API_SECRET } from "../utils/env.js";
 
+console.log("RAZORPAY_API_KEY:", RAZORPAY_API_KEY);
+console.log("RAZORPAY_API_SECRET:", RAZORPAY_API_SECRET);
+
 const instance = new Razorpay({
   key_id: RAZORPAY_API_KEY,
   key_secret: RAZORPAY_API_SECRET,
